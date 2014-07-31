1448a03c7dd32079d249128037a77e1bc6717c86
Kevin <mr.kev@me.com>
2014-07-30 23:11:12 -0500

    Draws! HELLZ YEAH! Draw the calendar, and allow meetings to be dragged arround to select sections.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global app, TowerModel, $*/
+/* global app, TowerModel, $, console*/
 
 /* Controllers */
 
@@ -13,7 +13,7 @@ app.controller('AppController', function ($scope, socket) {
     $scope.currentCalendar = $scope.model.calendars[$scope.model.term];
   };
 
-  $scope.$watch("model.savedata", function(newVal) {
+  $scope.$watch('model.savedata', function(newVal) {
       $scope.savedata = JSON.stringify(newVal);
   }, true);
 
@@ -31,20 +31,6 @@ app.controller('AppController', function ($scope, socket) {
   $scope.contains = function (element, array) {
     return array.find(element) > -1;
   };
-
-
-  //$scope.calendarCourse = function (object, add) {
-  //  console.log('calendarCourse');
-  //  var calarr = $scope.user.calendars[$scope._app.term];
-  //  if (add) {
-  //    object.awiwi = true;
-  //    calarr.push(object); 
-  //    return;
-  //  }
-  //
-  //  var delIndex = calarr.map(function(x) {return x.id; }).indexOf(object.id);
-  //  if (delIndex > -1) calarr.splice(delIndex, 1);
-  //};
 
   socket.on('connect',   function ()  {});
   socket.on('user info', function (info) { this.user = info; });
@@ -105,7 +91,7 @@ app.controller('SearchController', function ($scope, socket, $sce) {
 
 
 
-app.controller('CalendarController', function ($scope, socket) {
+app.controller('CalendarController', function ($scope, socket, $timeout) {
 
   /**
    * Query the courses we need.
@@ -193,12 +179,161 @@ app.controller('CalendarController', function ($scope, socket) {
 
   $scope.isSelectedSection = function (section) {
     return $scope.currentCalendar
-      .selectedSections[section.course_id][section.ssr_component] == section.class_number;
+      .selectedSections[section.course_id][section.ssr_component] 
+              == section.class_number;
+  };
+
+  $scope.ssrCompenentsOfType = function (course, ssr) {
+    return course.sections.map(function (x) {
+      return x.ssr_component;
+    }).reduce(function (prev, curr) {
+      return prev + (curr === ssr ? 1 : 0);
+    }, 0);
   };
 
 
-  // $scope.$watch('term.course_ids', function (newval, oldval) {
-  //     $scope.ccourses = $scope.model.term.course_ids;
-  // });
 
+  /////////////////////////////////
+  //
+  // Calendar drawing n stuff.
+  // 
+
+  // Drawing.
+  //   
+  
+  $scope.initCalendar = function () { console.log('init'); };
+
+  var width = 721;
+  var height = 1392;
+
+  var midnightMillis = function (time) {
+    return  Date.parse('July 26th, 2014, ' + time) - 
+        Date.parse('July 26th, 2014, 12:00AM');
+  };
+  
+  var table = 
+  { 'M' : 0,
+    'T' : 1,
+    'W' : 2,
+    'R' : 3,
+    'F' : 4,
+    'S' : 5  };
+
+  
+  $scope.getLeft = function (weekday) { 
+    return table[weekday] * $scope.getWidth(); 
+  };
+
+  $scope.getTop  = function (time) {
+    //console.log(midnightMillis(time));
+    return (midnightMillis(time) / 86400000 /* Milliseconds in a day */) * height >> 0;
+  };
+
+  $scope.getHeight = function (start, end) {
+    return $scope.getTop(end) - $scope.getTop(start);
+  };
+
+  $scope.getWidth = function () {
+    return width / Object.keys(table).length >> 0;
+  };
+
+
+  // Drag, drop.
+  // 
+
+
+  $scope.dragging_section = null;
+
+  var applyJQueryDrag = function () {
+    $('.draggable').draggable({
+
+      /**
+       * Items go back to original position when released.
+       * @type {Boolean}
+       */
+      revert : true,
+
+      /**
+       * No animation when items go back to original position.
+       * @type {Number}
+       */
+      revertDuration : 0,
+      
+      /**
+       * Show non-selected courses with the same ssr_component. This is done
+       * by setting $scope.dragging_ssr to the ssr of the section of the
+       * gathering being dragged.
+       * @param  {[type]} event [description]
+       * @param  {[type]} ui    [description]
+       * @return {[type]}       [description]
+       */
+      start : function (event, ui) {
+        // Show sections with same SSR.
+        console.log('Event?', event, 'UI', ui);
+        $scope.dragging_section = JSON.parse(event.target.dataset.section);
+        $scope.$apply();
+      },
+
+      /**
+       * [end description]
+       * @return {[type]} [description]
+       */
+      stop : function () {
+        $scope.dragging_section = null;
+        $scope.$apply();
+      }
+    });
+
+
+
+    $('.droppable').droppable({ 
+
+      /**
+       * Triggered when a draggable is dropped on top of this droppable. Selects
+       * this droppable's section. Updates angular and re-applies jquery 
+       * dragging.
+       *
+       * ui.draggable[0] = html element being dragged. Why is it an array?
+       * event.target    = element on which drop was recieved.
+       */
+      drop : function (event, ui) {
+
+        // Select the section
+        var section = JSON.parse(event.target.dataset.section);
+        
+        
+        // Update angular.
+        $scope.$apply(function () {
+          $scope.currentCalendar.selectSection(section);
+        });
+    
+        // Reapply draggable and droppable after digest cycle.
+        $timeout(function(){
+          applyJQueryDrag(); 
+        }); 
+      } 
+    });
+  };
+
+
+  // Whenever a repeat is done applies jquery dragging goodness.
+  $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
+    applyJQueryDrag();
+  });
+
+  
+});
+
+
+app.directive('onFinishRender', function ($timeout) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attr) {
+      if (scope.$last === true) {
+        $timeout(function () {
+          scope.$emit('ngRepeatFinished');
+        });
+      }
+    }
+  }
 });
---
+++ public/js/orwell.js
@@ -0,0 +1,69 @@
+'use strict';
+/* global $, console*/
+var YoCal = (function () {
+
+	function Orwell () {
+		this.width = 20;
+	}
+
+	var midnightMillis = function (time) {
+		return 	Date.parse('July 26th, 2014, ' + time) - 
+				Date.parse('July 26th, 2014, 12:00AM');
+	}
+
+	var table = 
+	{   'M'	: 0,
+		'T'	: 1,
+		'W'	: 2,
+		'R'	: 3,
+		'F'	: 4,
+		'S'	: 5 
+	}
+
+
+	var getLeft = function (weekday) { return table[weekday] * this.width; }
+	var getTop  = function (time) {
+		return (midnightMillis(time) / 86400000 /* Milliseconds in a day */) * this.height >> 0;
+	}
+
+	Orwell.prototype.gatherify = function(section) {
+
+		var results = new Array(section.meeting.meeting_pattern.length);
+		
+		var i = meeting.meeting_pattern.length;
+		while (i--) {
+		  console.log(meeting.meeting_pattern.charAt(i));
+		}
+
+	};
+
+	return Orwell;
+})();
+
+console.log('YoCal', YoCal);
+
+
+//  {
+// 	    building_code: "URH",
+// 	    room: "262",
+// 	    start_time: "11:40AM",
+// 	    end_time: "12:55PM",
+// 	    start_date: "08/26/2014",
+// 	    end_date: "12/05/2014",
+// 	    instructors: [
+// 	        "Lau,C (cyl58)"
+// 	    ],
+// 	    facility_description: "Uris Hall 262",
+// 	    meeting_pattern: "TR"
+// 	}
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
